7facc2ea45ae88188802f7f79380814793785fd4
Gerthasteam <laceiras.app@gmail.com>
2026-09-18 01:55:42 -0300

    Delete app directory

--- app/api/auth/logout/route.js
+++
@@ -1,5 +0,0 @@
-import { clearSessionCookie } from "../../../../lib/auth";
-
-export async function POST() {
-  return Response.json({ ok: true }, { headers: { "Set-Cookie": clearSessionCookie() } });
-}
